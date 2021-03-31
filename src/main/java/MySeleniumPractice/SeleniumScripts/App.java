31bd722addc4a002267973e598523a7cb3263b4c
sunny2403 <tadisunnyroy@gmail.com>
2021-03-31 11:56:22 +0530

    first commit

---
+++ src/main/java/MySeleniumPractice/SeleniumScripts/App.java
@@ -0,0 +1,13 @@
+package MySeleniumPractice.SeleniumScripts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/MySeleniumPractice/SeleniumScripts/SampleJunits.java
@@ -0,0 +1,19 @@
+package MySeleniumPractice.SeleniumScripts;
+
+import org.junit.Test;
+
+public class SampleJunits {
+
+	@Test   //test innotations
+	public void test() {
+		System.out.println("abc");
+	}
+	@Test   //test innotations
+	public void test1() {
+		System.out.println("abc");
+	}
+	@Test   //test innotations
+	public void test2() {
+		System.out.println("abc");
+	}
+}
